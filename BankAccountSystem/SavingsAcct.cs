b9119008477dc59cded70df1ced0e1973f91cf65
Emin Mammadhuseynov <emin.mov24@gmail.com>
2024-01-27 13:58:10 +0400

    Withdraw

--- BankAccountSystem/SavingsAcct.cs
+++ BankAccountSystem/SavingsAcct.cs
@@ -5,9 +5,13 @@
         private decimal InterestRate = 0.025m;
         private const decimal WithdrawalCharge = 2.0m;
         private const int WithdrawalLimit = 3;
+
+        private int withdrawalCount;
+
         public SavingsAcct(string firstname, string lastname, decimal interestrate, decimal balance) : base(firstname, lastname, balance)
         {
             InterestRate = interestrate;
+            withdrawalCount = 0;
         }
 
         public void ApplyInterest()
@@ -17,7 +21,7 @@
 
         public override void Withdraw(decimal amount)
         {
-            if (amount > WithdrawalLimit)
+            if (withdrawalCount >= WithdrawalLimit)
             {
                 Console.WriteLine($"More than {WithdrawalLimit} withdrawals! Extra withdrawal charge applied.");
                 Balance -= WithdrawalCharge;
